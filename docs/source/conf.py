7e47b46f7769ee2fa94d48455e8f3e08311bdd7f
wenfei999 <wfli@github.com>
2024-10-19 11:53:02 +0800

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,9 @@
 import os 
 import sys
 #sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../'))
+
 project = 'openaicg2'
 author = 'Lujun Zou'
 copyright = '2024 Lujun Zou, Wenfei Li'
